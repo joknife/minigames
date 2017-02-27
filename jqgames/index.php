87c14362d2e46009b03a76b1e255264cb7dd6c1e
joknife <sergmakeweb@gmail.com>
2017-02-27 16:30:59 +0500

    v 0.1 add text

--- jqgames/index.php
+++ jqgames/index.php
@@ -23,7 +23,8 @@
 			<button class = 'keys intro'>Описание</button>
 			<section id="intro">
 			<div id="intro_field">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin metus tortor, elementum id felis at, vulputate tincidunt ipsum. Vestibulum ornare eros vitae lacus sodales fermentum. Vestibulum dapibus elit vitae interdum lacinia. Quisque et diam a tortor scelerisque consectetur vel nec ante. Nunc sit amet orci nec elit ultricies tempor. Nulla tincidunt viverra diam vitae sodales. Aliquam at ultricies leo. Pellentesque quis diam et nulla tincidunt suscipit ac ac sem. Suspendisse pellentesque urna arcu, ac lacinia enim iaculis a. Pellentesque quis urna ac sem bibendum condimentum.</p>
+				<p>Этот сайт я сделал чтобы потреннироваться в программировании.
+				Это мой первый сайт с ипользованием JQuery.Полистал до этого несколько курсов по HTML,CSS и JS. Ну и PHP. Кроме раздела "логика"" все идеи я взял из заданий этих курсов. Реализовал неокторые задания на чистом JS, но на JS работать с DOM сейчас не практично, поэому почитал/посмотрел про JQuery и постарался использовать его. Реализовал все что задумывал на данном этапе. Дальше попробую PHP + AJAX. В планах минимальный движок с регистрацией, чат и сетевые крестики нолики. </p>
 			</div>
 			</section>
 
@@ -31,7 +32,7 @@
 			<button class = 'keys calc'>Калькулятор</button>
 			<section id="calc">
 				<div class="desc">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin metus tortor, elementum id felis at, vulputate tincidunt ipsum. Vestibulum ornare eros vitae lacus sodales fermentum. Vestibulum dapibus elit vitae interdum lacinia. Quisque et diam a tortor scelerisque consectetur vel nec ante. Nunc sit amet orci nec elit ultricies tempor. Nulla tincidunt viverra diam vitae sodales. Aliquam at ultricies leo. Pellentesque quis diam et nulla tincidunt suscipit ac ac sem. Suspendisse pellentesque urna arcu, ac lacinia enim iaculis a. Pellentesque quis urna ac sem bibendum condimentum.</p>
+					<p>Калькулятор на js проще простого. Думал придется парсить самому, потом ифы/кейзы, но лень, ведь цель немного не в том. Поэтому тупо eval()</p>
 				</div>
 				<div id="calc_field">
 				<div class = "calc_button" id="cls">C</div>
@@ -46,7 +47,7 @@
 			<button class = 'keys nums'>Цифры</button>
 			<section id="nums">
 			<div class="desc">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin metus tortor, elementum id felis at, vulputate tincidunt ipsum. Vestibulum ornare eros vitae lacus sodales fermentum. Vestibulum dapibus elit vitae interdum lacinia. Quisque et diam a tortor scelerisque consectetur vel nec ante. Nunc sit amet orci nec elit ultricies tempor. Nulla tincidunt viverra diam vitae sodales. Aliquam at ultricies leo. Pellentesque quis diam et nulla tincidunt suscipit ac ac sem. Suspendisse pellentesque urna arcu, ac lacinia enim iaculis a. Pellentesque quis urna ac sem bibendum condimentum.</p>
+			<p>На поле каждый раз по разному генерируются цифры от 1 до 25. Цель -  нажимать по очереди на клетки с цифрами по порядку от 1 и до 25 за заданное время. Нажатие мимо клетки со следующей цифрой сокращает время на 3с. Удачи!</p>
 			</div>
 			<div id="nums_field">
 				<input id="nums_screen" type="text" value="" disabled>
@@ -61,7 +62,7 @@
 			<button class = 'keys tictac'>Крестики нолики</button>
 			<section id="tictac">
 			<div class="desc" >
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin metus tortor, elementum id felis at, vulputate tincidunt ipsum. Vestibulum ornare eros vitae lacus sodales fermentum. Vestibulum dapibus elit vitae interdum lacinia. Quisque et diam a tortor scelerisque consectetur vel nec ante. Nunc sit amet orci nec elit ultricies tempor. Nulla tincidunt viverra diam vitae sodales. Aliquam at ultricies leo. Pellentesque quis diam et nulla tincidunt suscipit ac ac sem. Suspendisse pellentesque urna arcu, ac lacinia enim iaculis a. Pellentesque quis urna ac sem bibendum condimentum.</p>
+			<p>Классические крестики нолики 3х3. Хотел сделать многообразнее в плане размеров поля и условий выигрыша, но нет. Собираюсь реализовать это в сетевой игре.</p>
 			</div>
 			<div id="tictac_field">
 				<input id="tt_screen" type="text" value="" disabled></input>
@@ -74,7 +75,7 @@
 			<button class = 'keys logic'>Логика</button>
 			<section id="logic">
 			<div class="desc" >
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin metus tortor, elementum id felis at, vulputate tincidunt ipsum. Vestibulum ornare eros vitae lacus sodales fermentum. Vestibulum dapibus elit vitae interdum lacinia. Quisque et diam a tortor scelerisque consectetur vel nec ante. Nunc sit amet orci nec elit ultricies tempor. Nulla tincidunt viverra diam vitae sodales. Aliquam at ultricies leo. Pellentesque quis diam et nulla tincidunt suscipit ac ac sem. Suspendisse pellentesque urna arcu, ac lacinia enim iaculis a. Pellentesque quis urna ac sem bibendum condimentum.</p>
+			<p>Была у меня в детстве такая настольная игра. Дутство мое было так давно что играли в нее реально на столе без всяких компов. суть проста - нужно отгадать 4 цвета. Нажмая на пустые кружки вставляйте цвета. После нажатия готово выводится подсказка : черный кружок - этот цвет есть и он на этом месте; белый кружок - этот цвет есть, но на другом месте; серый кружок - этого цвета нет. Порядок кужков подсказки соответствует порядку в вашем ответе. Игра становится интересней если убрать это сооветсвие. То есть будет известно что какой то цвет на своем месте, какой то нет, но не будет известно какой. Будет время сделаю оба варианта..  </p>
 			</div>
 			<div id="logic_field">
 				<button id="logic_reset" class="buttons">Заново!</button>
