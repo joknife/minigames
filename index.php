6f5d17973e5643d68d535957ec598d195e2e1530
joknife <jiknife79@gmail.com>
2017-02-28 21:16:18 +0500

    freezzzzz

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>serg</title>
+	<meta charset="utf-8">
+	<title>Main page</title>
 </head>
 <body>
-<?php phpinfo() ?>
+<a href="jqgames/index.php">Mini games</a>
+<style type="text/css">
+	body{
+		color: #eee;
+		background-color: #777;
+	}
+	a {
+		display: block;
+		width: 280px;
+		margin: 20px auto;
+		border-radius: 0.5em 0.5em 0.5em 0.5em;
+		background-color: #444; 
+		color: #D9EEF7;
+	    cursor: pointer;
+	    font: 18px/100% Arial,Helvetica,sans-serif;
+	    outline: medium none;
+	    padding: 0.5em 2em 0.55em;
+	    text-align: center;
+	    text-decoration: none;
+	    text-shadow: 0 1px 1px rgba(0, 0, 0, 0.3);
+	    vertical-align: baseline;
+	}
+</style>
 </body>
 </html>
